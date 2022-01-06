ff3d8c144eb1b222c7230bb934e15c40afb3edc1
Sander <sendongliang@gmail.com>
2022-01-06 15:56:12 +1300

    add procfile

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from website import creat_app
 import os
-
+port = int(os.environ.get('PORT', 5000))
 app = creat_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
